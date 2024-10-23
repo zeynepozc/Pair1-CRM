05e3997a27ae261363e13afec2031a63c7694d22
Çağatay <muhammedcagataysero34@gmail.com>
2024-10-23 16:38:45 +0300

    core

---
+++ backend/core/src/main/java/io/github/cagataysero/exception/response/BusinessExceptionResponse.java
@@ -0,0 +1,15 @@
+package io.github.cagataysero.exception.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusinessExceptionResponse {
+    private String message;
+}
--- backend/core/src/main/java/io/github/cagataysero/security/BaseJwtService.java
+++ backend/core/src/main/java/io/github/cagataysero/security/BaseJwtService.java
@@ -8,7 +8,8 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
-
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -20,11 +21,12 @@ public class BaseJwtService {
     private String SECRET_KEY = "cx4YnoSF1vOn3DCTpckRFlhhuSMWM1reEOrnoGQMrFUlE5kLCvPWaORnZ3hkkCxI7rf1YAznsTn5uM0detQksaFcCDGVHDVQzNB20Wu0EEIDxdlvfdlnAgryUtN7sQyTnmdzu5aM2pg9MKR8OKUHfMZkwEDjVXHuxsI9vRKnYoCnfiQv5Hv4SlC";
 
 
-    public String generateToken(String userName) {
+    public String generateToken(String userName, List<String> roles) {
         return Jwts
                 .builder()
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .claim("roles", roles)
                 .subject(userName)
                 .signWith(getSignKey())
                 .compact();
@@ -44,6 +46,12 @@ public class BaseJwtService {
     public String extractUsername(String token)
     {
         return getClaimsFromToken(token).getSubject();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> getRolesFromToken(String token) {
+        Claims claims = getClaimsFromToken(token);
+        return claims.get("roles", List.class);
     }
 
     private Claims getClaimsFromToken(String token)
