ab60231a2fca66926973e787fc753cdecbd51375
Çağatay <muhammedcagataysero34@gmail.com>
2024-11-06 12:53:21 +0300

    .

--- backend/productservice/src/main/java/com/etiya/productservice/consumer/ProductConsumer.java
+++ backend/productservice/src/main/java/com/etiya/productservice/consumer/ProductConsumer.java
@@ -1,22 +1,22 @@
-package com.etiya.productservice.consumer;
-
-import com.etiya.event.ProductsCalledEvent;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
-import java.util.function.Consumer;
-
-@Component
-@Slf4j
-public class ProductConsumer {
-
-    @Bean
-    public Consumer<ProductsCalledEvent> productsCalledEvent()
-    {
-        return event -> {
-            log.info("Customerservice'ten openfeign ve kafka kullanıldı, stream'den alındı. CustomerId:" + event.getId());
-        };
-    }
-}
+//package com.etiya.productservice.consumer;
+//
+//import com.etiya.event.ProductsCalledEvent;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.function.Consumer;
+//
+//@Component
+//@Slf4j
+//public class ProductConsumer {
+//
+//    @Bean
+//    public Consumer<ProductsCalledEvent> productsCalledEvent()
+//    {
+//        return event -> {
+//            log.info("Customerservice'ten openfeign ve kafka kullanıldı, stream'den alındı. CustomerId:" + event.getId());
+//        };
+//    }
+//}
